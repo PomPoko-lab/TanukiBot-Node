85235c7d6d587e1504161b3d965a9f130bc186a2
ckennx <ckennx@gmail.com>
2022-07-20 15:05:03 -0500

    changes to holiday feature

--- features/Holiday.ts
+++ features/Holiday.ts
@@ -9,7 +9,7 @@ export default async (client: Client) => {
   const country = 'US';
   const currYear = new Date().getFullYear();
   const currMonth = new Date().getMonth() + 1;
-  const currDay = +format(addDays(new Date(), 1), 'd');
+  const currDay = new Date().getDay();
   const days3 = +format(addDays(new Date(), 4), 'd');
   const params = `&country=${country}&year=${currYear}&month=${currMonth}`;
 
