5b54d0d6b8ef22f02b9e4cc7af33adcdb0a35645
Kenny Cao <ckennx@gmail.com>
2023-11-17 20:29:21 -0600

    Convert ServerHandler and serverlist to js files

---
+++ src/classes/ServerHandler.js
@@ -0,0 +1,46 @@
+import { exec } from "child_process";
+import find from "find-process";
+
+export class ServerHandler {
+    /**
+     * 
+     * @param {string} processName 
+     * @param {string} filePath 
+     * @param {string} fileExe 
+     */
+    constructor(processName, filePath, fileExe) {
+        this.processName = processName;
+        this.filePath = filePath;
+        this.fileExe = fileExe;
+    }
+
+    getServerinfo() {
+        return {
+            processName: this.processName,
+            filePath: this.filePath,
+            fileExe: this.fileExe,
+        };
+    }
+
+    async executeFile() {
+        const active = await this.checkProcess(this.processName);
+
+        if (active) return false;
+
+        exec(`"${this.filePath}${this.fileExe}"`);
+        return true;
+    }
+
+    // Can be used to check if any process is running or the current server's process
+    /**
+     * 
+     * @param {string} processName 
+     * @returns 
+     */
+    async checkProcess(processName) {
+        const results = await find("name", processName || this.processName);
+        const exists = results.length > 0;
+
+        return exists;
+    }
+}
